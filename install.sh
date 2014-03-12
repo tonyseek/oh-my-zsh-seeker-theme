1b3082fca9b9ce3204f24b99d6bae760bdaa23e6
TonySeek <tonyseek@gmail.com>
2014-03-12 12:04:56 +0800

    add script to install theme quickly.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+repo="git://github.com/tonyseek/oh-my-zsh-seeker-theme.git"
+target="$HOME/.oh-my-zsh-themes/seeker"
+
+if [ -d "$target" ]; then
+    if [ -d "$target/.git" ]; then
+        (cd $target && git pull --ff-only origin master)
+    fi
+else
+    git clone $repo $target
+fi
+
+ln -sf $target/seeker.zsh-theme $HOME/.oh-my-zsh/themes/seeker.zsh-theme
+ln -sf $target/seeker-tiny.zsh-theme $HOME/.oh-my-zsh/themes/seeker-tiny.zsh-theme
